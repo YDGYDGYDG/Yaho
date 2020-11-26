eff9add5351c8a62d73c390ee92609c336c3b4a3
YDGYDGYDG <tlsaoss@naver.com>
2020-11-26 20:54:23 +0900

    인트로 이후 UI 다시 그려지는거 수정

--- YahoTest/Scripts/UI.lua
+++ YahoTest/Scripts/UI.lua
@@ -1,8 +1,4 @@
 
-ScreenUI.hpBarVisible  = false
-ScreenUI.mpBarVisible  = false
-ScreenUI.expBarVisible  = false
-ScreenUI.levelVisible = false
 
 HPMP = Image("Pictures/HPMP.png", Rect(Client.width/2-104, Client.height-101, 207, 41))
 HUD = Image("Pictures/HUD.png", Rect(0, Client.height-48, 1200, 48))
@@ -27,7 +23,11 @@ LVText.rect = Rect(10, Client.height-48+18, 122, 31)
 LVText.textSize = 20
 
 function refreshUI()
-
+	ScreenUI.hpBarVisible  = false
+	ScreenUI.mpBarVisible  = false
+	ScreenUI.expBarVisible  = false
+	ScreenUI.levelVisible = false
+	
 	LVText.text = "LV."..Client.myPlayerUnit.level
 	LVTextOut1.text = "<color=#000000>LV."..Client.myPlayerUnit.level.."</color>"
 	LVTextOut2.text = "<color=#000000>LV."..Client.myPlayerUnit.level.."</color>"
