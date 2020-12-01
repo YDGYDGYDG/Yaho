c824379fb956a270766db47ae6f99ea9d448180a
hakyung1227 <74641325+hakyung1227@users.noreply.github.com>
2020-12-02 01:56:40 +0900

    백업용
    백업

---
+++ YahoTest - 복사본/ServerScripts/SliderHit_Server.lua
@@ -0,0 +1,15 @@
+
+Server.GetTopic("SlliderHitResult").Add(function(ATKResult)
+    Server.damageCallback = function(a, b, damage, SkillID)
+        local thisSkillID = SkillID
+        --'일섬'이면 이 공식 적용
+        if thisSkillID == 4 then
+            -- return ATKResult
+            return a.atk * ATKResult - b.def
+        end
+        return a.atk - b.def
+    end
+    -- unit.SendSay(ATKResult)
+    Server.FireEvent("UseSkill", 4)
+end)
+
